81578062580c1c321421794892a966a0f84c05ac
DavidWells <hello@davidwells.io>
2019-10-08 21:11:01 -0700

    update test

--- examples/stubbing-spying__google-analytics/cypress/integration/google-analytics-stubbing.js
+++ examples/stubbing-spying__google-analytics/cypress/integration/google-analytics-stubbing.js
@@ -1,18 +1,7 @@
 /* global Cypress, cy */
 
-// in our cypress.json file we have blacklisted www.google-analytics.com
-// which prevents the GA script from ever loading. however because there
-// is still a global 'window.ga' function, that means we can stub it
-// and ensure its called correctly.
-//
-// if you pop open your dev tools you will see that the network request
-// for the script tag returns 503 because it's been blacklisted.
+// Taken from https://github.com/cypress-io/cypress-example-recipes/tree/master/examples/stubbing-spying__google-analytics
 
-// using a global event handler here because likely
-// in your real app you'll always want to stub window.ga
-//
-// if not you could just add a onBeforeLoad() callback
-// to the cy.visit
 Cypress.on('window:before:load', (win) => {
   // because this is called before any scripts
   // have loaded - the ga function is undefined
@@ -20,36 +9,52 @@ Cypress.on('window:before:load', (win) => {
   // win.ga = cy.stub().as('ga')
 })
 
+// https://docs.cypress.io/guides/guides/stubs-spies-and-clocks.html#Stubs
+
 describe('Google Analytics', function () {
   beforeEach(function () {
     cy.visit('/index.html')
-    // cy.window().should('have.property', 'appReady', true)
   })
 
   it('can ensure window.ga is called correctly', function () {
     // cy.wait(500)
     // wait until a global variable has an expected value
-    cy.waitUntil(() => cy.window().then(win => win.foo === "bar"));
 
+    cy.waitUntil(() => cy.window().then((win) => {
+      // Wait for real google analytics to load
+      return (typeof win.ga === 'function')
+    }))
 
-    // cy
-    // .get('@ga')
-    // // ensure GA was created with our google analytics ID
-    // .should('be.calledWith', 'create', 'UA-XXXXX-Y')
-    // // and ensure that the initial pageview was sent
-    // .and('be.calledWith', 'send', 'pageview')
+    /*
+      Attach spy to real google analytics? win.ga
 
-    // // now click the anchor tag which causes a hashchange event
+      But how? 🤔
+     */
+
+    // const spy = cy.spy(obj, 'foo').as('anyArgs')
+
+    cy
+      .get('@ga')
+      // ensure GA was created with our google analytics ID
+      .should('be.calledWith', 'create', 'UA-126647663-3', 'auto')
+      // and ensure that the initial pageview was sent
+      .and('be.calledWith', 'send', 'pageview')
+
+    // now click the anchor tag which causes a hashchange event
     cy.contains('#page2').click()
 
-    // cy.hash().should('equal', '#page2')
+    cy.hash().should('equal', '#page2')
 
-    // // make sure GA was sent this pageview
-    // cy.get('@ga').should('be.calledWith', 'send', 'pageview', '#page2')
+    // make sure GA was sent this pageview
+    cy.get('@ga')
+      .should('be.calledWith', 'set', 'page')
+      .and('be.calledWith', 'send', 'pageview')
 
-    // // and now do it again for page3
-    // cy.contains('#page3').click()
-    // cy.hash().should('equal', '#page3')
-    // cy.get('@ga').should('be.calledWith', 'send', 'pageview', '#page3')
+    // and now do it again for page3
+    cy.contains('#page3').click()
+    cy.hash().should('equal', '#page3')
+    cy.get('@ga')
+      .should('be.calledWith', 'set', 'page')
+      .and('be.calledWith', 'send', 'pageview')
   })
 })
